b5ccb56a3c2583781272f17ce025535909b7bf64
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-07 21:27:08 +0800

    Update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/Header/Header"
+import Home from "./components/Contents/Home"
 
 const App = () => {
   return(
-    <Header />
+    <>
+      <Header />
+      <Home />
+    </>
   )
 }
 
