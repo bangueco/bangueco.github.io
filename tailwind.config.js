6210513359e0c4b1ba866a3b842317f1fdb7866d
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-08 18:25:10 +0800

    Add custom animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slide_left: {
+          from: {left: '100px'},
+          to: {left: '0px'}
+        },
+      },
+      animation: {
+        'sliding-left': 'slide_left 2s ease normal'
+      }
+    },
   },
   plugins: [],
 }
