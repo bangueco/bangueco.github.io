21d7bff04178ee32ac4be7d1aa4a104b179c9515
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-10 22:08:50 +0800

    Update header link navigations

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -24,13 +24,13 @@ const Header = () => {
           <nav className={`md:flex md:items-center md:w-auto text-center bg-zinc-900 md:static w-full absolute transition-all duration-300 ease-in-out ${isOpen ? 'left-[0px]' : 'left-[-900px]'}`}>
             <ul className="flex flex-col md:flex-row md:items-center md:gap-3 gap-8 md:mt-0 mt-5 p-2">
               <li>
-                <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#">Home</a>
+                <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#home">Home</a>
               </li>
               <li>
-                <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#">About</a>
+                <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#about">About</a>
               </li>
               <li>
-                <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#">Projects</a>
+                <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#projects">Projects</a>
               </li>
               <li>
                 <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#">Services</a>
