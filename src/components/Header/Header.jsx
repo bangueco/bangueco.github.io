aa1b5cdfc3a5049f2dc9b0ac686cbedf59c08508
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-10 22:57:45 +0800

    Fix header? extending through out of screen

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
 
   return(
     <>
-      <header className="fixed top-0 min-w-full p-3 z-[1100] bg-zinc-900">
+      <header className="fixed top-0 min-w-screen p-3 z-[1100] bg-zinc-900">
         <div className="md:flex md:items-center md:justify-between relative">
           <div className="icon text-green-300 text-3xl p-3 self-start">bangueco()</div>
           <div className="md:hidden absolute right-0 top-3">
@@ -21,7 +21,7 @@ const Header = () => {
               <RxHamburgerMenu size={30} />
             </button>
           </div>
-          <nav className={`md:flex md:items-center md:w-auto text-center bg-zinc-900 md:static w-full absolute transition-all duration-300 ease-in-out ${isOpen ? 'left-[0px]' : 'left-[-900px]'}`}>
+          <nav className={`md:flex md:items-center text-center bg-zinc-900 md:static w-full absolute transition-all duration-300 ease-in-out ${isOpen ? 'left-[0px]' : 'left-[-900px]'}`}>
             <ul className="flex flex-col md:flex-row md:items-center md:gap-3 gap-8 md:mt-0 mt-5 p-2">
               <li>
                 <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#home">Home</a>
