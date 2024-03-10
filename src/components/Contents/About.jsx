cfa671fa27ee6bf48c727c98338c5e4eeaed753b
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-10 23:16:47 +0800

    Fix attempt 2

--- src/components/Contents/About.jsx
+++ src/components/Contents/About.jsx
@@ -3,14 +3,16 @@ const About = () => {
     <section id="about" className="min-h-screen p-1">
       <div className="mt-32 flex justify-center items-center flex-col gap-12 p-5">
         <h1 className="text-4xl text-green-300 lg:text-6xl">About Me</h1>
-        <p className="text-lg text-nowrap w-4/5 text-gray-300 sm:w-4/5 sm:text-xl md:w-4/6 lg:w-4/6 lg:text-2xl">
-          I&apos;m a 21 y/o, 3rd year college student from Eastwoods Professional College of Science and Technology,
-          with a strong passion for web development.
-        </p>
-        <p className="text-lg text-nowrap w-4/5 text-gray-300 sm:w-4/5 sm:text-xl md:w-4/6 lg:w-4/6 lg:text-2xl">
-          While having solid foundation in back-end development, I also enjoy learning front-end technologies, making me a versatile full-stack developer. Delving into different frameworks, 
-          learning new tools, and mastering various languages in-order to enhance my skills and knowledge further.
-        </p>
+        <div className="w-full">
+          <p className="text-lg text-nowrap w-4/5 text-gray-300 sm:w-4/5 sm:text-xl md:w-4/6 lg:w-4/6 lg:text-2xl">
+            I&apos;m a 21 y/o, 3rd year college student from Eastwoods Professional College of Science and Technology,
+            with a strong passion for web development.
+          </p>
+          <p className="text-lg text-nowrap w-4/5 text-gray-300 sm:w-4/5 sm:text-xl md:w-4/6 lg:w-4/6 lg:text-2xl">
+            While having solid foundation in back-end development, I also enjoy learning front-end technologies, making me a versatile full-stack developer. Delving into different frameworks, 
+            learning new tools, and mastering various languages in-order to enhance my skills and knowledge further.
+          </p>
+        </div>
         <h1 className="text-2xl text-green-300 lg:text-4xl mt-10">Current Tech Stack</h1>
         <div className="grid grid-cols-3 gap-5 text-center lg:grid-cols-5">
           <p className="border-green-300 border-2 w-28 text-lg rounded-lg text-gray-300 font-bold">HTML</p>
