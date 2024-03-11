ebb1099645bbb9cb67c3fb5b0e66776685058fcc
bangueco <121280104+bangueco@users.noreply.github.com>
2024-03-11 11:47:14 +0800

    Rewrite header for mobile

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ const App = () => {
   return(
     <>
       <Header />
-      <Home />
-      <About />
-      <Projects />
+      <main>
+        
+      </main>
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,46 +1,37 @@
-import { useState } from "react";
-import { RxHamburgerMenu } from "react-icons/rx";
+import { RxHamburgerMenu } from "react-icons/rx"
+import { useState } from "react"
 
 const Header = () => {
-  const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  const [isOpen, setIsOpen] = useState(false)
 
   return(
     <>
-      <header className="fixed top-0 w-screen p-3 z-[1100] bg-zinc-900">
-        <div className="md:flex md:items-center md:justify-between relative">
-          <div className="icon text-green-300 text-3xl p-3 self-start">bangueco()</div>
-          <div className="md:hidden absolute right-0 top-3">
-            <button
-              onClick={toggleMenu}
-              className="text-gray-50 focus:outline-none focus:text-green-400"
-            >
-              <RxHamburgerMenu size={30} />
-            </button>
-          </div>
-          <nav className={`md:flex md:items-center md:w-auto text-center bg-zinc-900 md:static w-full absolute transition-all duration-300 ease-in-out ${isOpen ? 'left-[0px]' : 'left-[-900px]'}`}>
-            <ul className="flex flex-col md:flex-row md:items-center md:gap-3 gap-8 md:mt-0 mt-5 p-2">
-              <li>
-                <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#home">Home</a>
-              </li>
-              <li>
-                <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#about">About</a>
-              </li>
-              <li>
-                <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#">Projects</a>
-              </li>
-              <li>
-                <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#">Services</a>
-              </li>
-              <li>
-                <a className="text-xl text-gray-50 p-3 md:hover:border-b-2 hover:border-green-300 transition duration-500 hover:text-green-300" href="#">Contact</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
+      <header className="relative bg-zinc-700">
+        <div className="p-2 text-xl text-green-300">bangueco()</div>
+        <ul className={`flex justify-center flex-col items-center gap-2 absolute bg-zinc-500
+          z-[-1] p-2 w-full ${isOpen ? 'top-11 opacity-100' : 'top-[-490px]'} opacity-0 transition-all duration-500 ease-in-out`}>
+          <li>
+            <a href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#projects">
+              Projects
+            </a>
+          </li>
+        </ul>
+        <RxHamburgerMenu 
+          onClick={() => setIsOpen(!isOpen)} 
+          className="absolute top-3 right-3 text-gray-100" 
+          size={25} 
+        />
       </header>
     </>
   )
