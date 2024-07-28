927c695dbbab7406f67fd422813077aaa4e7cb9f
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-28 16:47:09 +0800

    feat: add welcome content section

---
+++ src/components/Contents/Welcome.tsx
@@ -0,0 +1,18 @@
+import './contents.css'
+import hi from '../../assets/images/hi.png'
+
+export function Welcome() {
+  return (
+    <section id="welcome-section">
+      <div className="section-container">
+        <img id='welcome-avatar' src={hi} alt="hi hello" />
+        <div id='welcome-introduction'>
+          <p>Hello, I'm Justine Ivan Gueco</p>
+          <div>
+            <p>I'm a fullstack developer, with an expertise in web and mobile development. I am fully commited to delivering high quality projects from scratch.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
