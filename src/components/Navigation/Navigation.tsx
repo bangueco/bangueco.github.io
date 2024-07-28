cf35754f7099a27d048fda2a0e6fb3a977f8f163
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-28 13:56:54 +0800

    feat: add navigation header

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,19 @@
+import './Navigation.css'
+
+function Navigation() {
+  return (
+    <header id='header-container'>
+      <p>bangueco()</p>
+      <nav>
+        <ul>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+      <p>Download CV</p>
+    </header>
+  )
+}
+
+export default Navigation
