ec89dfb978997d9e9cdc55b67c09c4c709a1727c
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-30 19:53:48 +0800

    refactor: user interface and responsiveness

--- src/components/Contents/Welcome.tsx
+++ src/components/Contents/Welcome.tsx
@@ -1,7 +1,9 @@
 import hi from '../../assets/images/hi.png'
 import { motion } from 'framer-motion'
+import TypingAnimation from '../Animation/TypingAnimation'
 
 export default function Welcome() {
+
   return (
     <section id="welcome-section">
       <div className="section-container">
@@ -9,20 +11,35 @@ export default function Welcome() {
           id='welcome-avatar' 
           src={hi} 
           alt="hi hello"
-          initial={{opacity: 0, zIndex: -1}}
-          animate={{opacity: 1, zIndex: -1}}
-          transition={{type: 'spring', duration: 3}}
+          initial={{x: -200, zIndex: -1}}
+          animate={{x: 0, zIndex: -1}}
+          transition={{type: 'spring', duration: 2}}
         />
-        <motion.div 
-          id='welcome-introduction'
-          initial={{opacity: 0, scale: 0.3}}
-          animate={{opacity: 1, scale: 1, zIndex: -1}}
-          transition={{duration: 0.8}}
+        <motion.div
+          id='waving-hand'
+          initial={{x: 200}}
+          animate={{x: 0}}
+          transition={{type: 'spring', duration: 2}}
         >
-          <p>Hello, I'm Justine Ivan Gueco</p>
           <div>
-            <p>I'm a fullstack developer, with an expertise in web and mobile development. I am fully commited to delivering high quality projects from scratch.</p>
+            <motion.p
+              initial={{ rotate: 0 }}
+              animate={{ rotate: [0, 20, -20, 0] }}
+              transition={{ repeat: Infinity, duration: 1.3, repeatDelay: 2.5 }}
+            >
+              👋🏼
+            </motion.p>
+            <p>Hey there! I am..</p>
           </div>
+          <TypingAnimation text="Justine Ivan Gueco" />
+          <motion.div
+            initial={{opacity: 0}}
+            animate={{opacity: 1}}
+            transition={{duration: 3}}
+          >
+            <button>Download CV</button>
+            <button>Scroll Down</button>
+          </motion.div>
         </motion.div>
       </div>
     </section>
