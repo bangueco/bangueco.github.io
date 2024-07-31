bbb5415d925e05ffe7d55661f5e4c82be69013ec
bangueco <121280104+bangueco@users.noreply.github.com>
2024-07-31 20:09:31 +0800

    refactor: new animation for sidebar

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,16 +1,23 @@
 import { useEffect, useState } from 'react'
 import './Navigation.css'
-import { RxHamburgerMenu } from "react-icons/rx";
-import Sidebar from './Sidebar';
+import { RxCross1, RxHamburgerMenu } from "react-icons/rx";
+import { AnimatePresence, motion } from "framer-motion"
 
 function Navigation() {
 
   const [scrolled, setScrolled] = useState<number>(0)
+  const [sidebarVisible, setSidebarVisible] = useState<boolean>(false)
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      setScrolled(window.scrollY)
-    })
+    const handleScroll = () => {
+      setScrolled(window.scrollY);
+    };
+  
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
   }, [])
 
   useEffect(() => {
@@ -24,15 +31,7 @@ function Navigation() {
   }, [scrolled])
 
   const toggleMenu = () => {
-    const headerMenu = document.querySelector('#sidebar')
-
-    if (headerMenu && !headerMenu.classList.contains('header-active')) {
-      headerMenu.classList.toggle('hidden')
-      headerMenu.classList.toggle('sidebar-active')
-    } else {
-      headerMenu?.classList.toggle('hidden')
-      headerMenu?.classList.toggle('sidebar-active')
-    }
+    setSidebarVisible(!sidebarVisible)
   }
 
   return (
@@ -40,13 +39,41 @@ function Navigation() {
       <div id="header-container">
         <p>bangueco()</p>
         <nav>
-          <a href="#">ABOUT</a>
+          <a href="#about">ABOUT</a>
           <a href="#">PROJECTS</a>
           <a href="#">CONTACT</a>
         </nav>
-        <RxHamburgerMenu id='hamburger-menu' onClick={toggleMenu} color='white' size={30} />
+        {
+          !sidebarVisible 
+          ? <RxHamburgerMenu id='hamburger-menu' onClick={toggleMenu} color='white' size={30} />
+          : <RxCross1 onClick={toggleMenu} color='white' size={30} />
+        }
+        <AnimatePresence>
+          {
+            sidebarVisible && (
+            <motion.div 
+              id='sidebar'
+              initial={{translateX: 200}}
+              animate={{translateX: 0}}
+              exit={{translateX: 200}}
+              transition={{duration: 0.65}}
+            >
+              <motion.div 
+                id="sidebar-menu"
+                initial={{opacity: 0}}
+                animate={{opacity: 1}}
+                exit={{opacity: 0}}
+                transition={{duration: 0.1}}
+              >
+                <a href="#about">About</a>
+                <a href="">Projects</a>
+                <a href="">Contact</a>
+              </motion.div>
+            </motion.div>
+            )
+          }
+        </AnimatePresence>
       </div>
-      <Sidebar />
     </header>
   )
 }
