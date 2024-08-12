76948e12dd90750ef2191d250129d2d53847c904
bangueco <121280104+bangueco@users.noreply.github.com>
2024-08-12 12:12:09 +0800

    feat: add things i do data

---
+++ src/lib/data.ts
@@ -0,0 +1,30 @@
+const thingsIDoData = [
+  {
+    title: "# Frontend",
+    description: "I develop user interface with frontend technologies, while prioritizing simplicity, ease-of-use and user friendliness."
+  },
+  {
+    title: "# Backend",
+    description: "I develop and maintain server-side applications, ensuring efficient data handling and robust functionality."
+  },
+  {
+    title: "# Mobile Development",
+    description: "I develop native mobile applications for Android and iOS with React Native."
+  },
+  {
+    title: "# Testing",
+    description: "I do unit and api testing ensuring those functionalities are reliable to be delivered to production environment."
+  },
+  {
+    title: "# Debugging",
+    description: "I identify and resolve bugs by analyzing error reports, debugging code, and applying effective solutions."
+  },
+  {
+    title: "# Deployment",
+    description: "I deploy applications using Docker, Vercel and Apache Web Server/Nginx."
+  }
+];
+
+export {
+  thingsIDoData
+}
