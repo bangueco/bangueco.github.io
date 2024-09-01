39b38e0bda6461669d8d4269fd6021eb327c8a8c
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-01 23:18:11 +0800

    feat: small tweaks on animation

--- src/components/About/ThingsIDo.tsx
+++ src/components/About/ThingsIDo.tsx
@@ -6,13 +6,13 @@ import { Card, CardHeader, CardBody, Divider } from "@nextui-org/react"
 
 export default function ThingsIDo() {
   return (
-    <article className="pt-20 flex justify-center items-center flex-col gap-5">
+    <article className="pt-40 flex justify-center items-center flex-col gap-5">
       <motion.h1
         className="text-primary text-3xl md:text-4xl lg:text-5xl font-bold"
         initial={{x: -20, opacity: 0, filter: 'blur(5px)'}}
         whileInView={{x: 0, opacity: 1, filter: 'blur(0px)'}}
-        transition={{duration: 0.4}}
-        viewport={{once: true}}
+        transition={{duration: 0.8}}
+        viewport={{once: true, amount: 'all'}}
       >
         Things I Do
       </motion.h1>
@@ -21,10 +21,10 @@ export default function ThingsIDo() {
           thingsIDo.map((item, index)=> (
             <motion.div
               key={index}
-              initial={{x: -50, opacity: 0, filter: 'blur(5px)'}}
+              initial={{x: -20, opacity: 0, filter: 'blur(5px)'}}
               whileInView={{x: 0, opacity: 1, filter: 'blur(0px)'}}
-              transition={{ duration: 0.3, delay: index * 0.1 }}
-              viewport={{once: true}}
+              transition={{ duration: 0.5, delay: index * 0.2 }}
+              viewport={{once: true, amount: 'all'}}
             >
               <Card className='h-40'>
                 <CardHeader className='text-3xl font-bold'>
