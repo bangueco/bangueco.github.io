f277da7b2e4c4e80f7996433db0c7bcb63b29e54
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-02 19:41:33 +0800

    feat: add project lists component

---
+++ src/components/Projects/ListOfProjects.tsx
@@ -0,0 +1,31 @@
+import AnimatedH1 from "../AnimatedH1";
+import { projects } from "@/lib/projects";
+import ProjectCard from "./ProjectCard";
+import AnimatedP from "../AnimatedP";
+
+export default function ListOfProjects() {
+  return (
+    <>
+      <AnimatedH1>My Projects</AnimatedH1>
+      <AnimatedP>
+        A list of my personal and school projects, more info can be found at my <a className="text-primary" href="https://www.github.com/bangueco">Github</a>
+      </AnimatedP>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+        {
+          projects.map((project, index) => (
+            <ProjectCard
+              key={index}
+              index={index}
+              url={project.url}
+              title={project.title}
+              description={project.description}
+              technologies={project.technologies}
+              repo_url={project.repo_url}
+              live_preview={project.live_preview}
+            />
+          ))
+        }
+      </div>
+    </>
+  )
+}
