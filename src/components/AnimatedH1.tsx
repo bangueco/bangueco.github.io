e22b0b4b02499ccf72311176a39e663722f79120
bangueco <121280104+bangueco@users.noreply.github.com>
2024-09-02 19:39:53 +0800

    feat: add animated header 1 component

---
+++ src/components/AnimatedH1.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { title } from '@/lib/animationVariants'
+import {motion} from 'framer-motion'
+import { ReactNode } from 'react'
+
+export default function AnimatedH1({children}: {children: ReactNode}) {
+  return (
+    <motion.h1
+      className="text-primary text-3xl md:text-4xl lg:text-5xl font-bold"
+      initial="hidden"
+      whileInView="visible"
+      viewport={{once: true, amount: 'all'}}
+      variants={title}
+    >
+      {children}
+    </motion.h1>
+  )
+}
