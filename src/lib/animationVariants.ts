68dfb6572951eabc06dc3866c9748b54070a64a6
bangueco <121280104+bangueco@users.noreply.github.com>
2024-12-17 23:47:47 +0800

    chore: improve animation time and remove some projects

--- src/lib/animationVariants.ts
+++ src/lib/animationVariants.ts
@@ -1,12 +1,12 @@
 import { Variants } from "framer-motion";
 
 export const title: Variants = {
-  hidden: {x: -20, opacity: 0, filter: 'blur(5px)'},
+  hidden: {x: -5, opacity: 0, filter: 'blur(5px)'},
   visible: {
     x: 0, 
     opacity: 1, 
     filter: 'blur(0px)',
-    transition: { duration: 1 }
+    transition: { duration: 0.3 }
   },
 }
 
@@ -15,8 +15,8 @@ export const itemContainer: Variants = {
   reveal: { 
     opacity: 1,
     transition: {
-      duration: 0.3,
-      staggerChildren: 0.5
+      duration: 0.2,
+      staggerChildren: 0.2
     }
   }
 }
@@ -33,20 +33,21 @@ export const itemFlipContainer: Variants = {
 }
 
 export const itemFadeReveal: Variants = {
-  hidden: {x: -20, opacity: 0, filter: 'blur(5px)'},
+  hidden: {scale: 0.1, opacity: 0, filter: 'blur(5px)'},
   reveal: {
-    x: 0, 
+    scale: 1, 
     opacity: 1, 
     filter: 'blur(0px)',
   }
 }
 
 export const itemFlipReveal: Variants = {
-  hidden: {x: -30, scaleX: 0.3, opacity: 0, filter: 'blur(5px)'},
+  hidden: {x: -5, scaleX: 0.2, opacity: 0, filter: 'blur(5px) brightness(0.7) hue-rotate(90deg)'},
   reveal: {
     x: 0, 
-    scaleX: 1, 
-    opacity: 1, 
-    filter: 'blur(0px)',
+    scaleX: 1,
+    rotateY: 0,
+    opacity: 1,
+    filter: 'blur(0px) brightness(1) hue-rotate(0deg)',
   }
 }
--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -30,20 +30,5 @@ export const projects: Array<ProjectsProps> = [
     description: "One of my school activity in my second year 1st sem midterm requirement for DBMS subject, a simple inventory management system for our computer laboratory.",
     technologies: [{tech: "HTML"}, {tech: "CSS"}, {tech: "PHP"}, {tech: "Laravel"}, {tech: "Bootstrap"}, {tech: "MySQL"}],
     repo_url: "https://www.github.com/bangueco/inventory-system"
-  },
-  {
-    url: "/images/projects/unavailable-image.jpg",
-    title: "Event Registration System",
-    description: "One of my school activity in my second year 1st sem finals requirement for DBMS subject, a simple event registration management system.",
-    technologies: [{tech: "HTML"}, {tech: "CSS"}, {tech: "PHP"}, {tech: "Laravel"}, {tech: "Bootstrap"}, {tech: "MongoDB"}],
-    repo_url: "https://www.github.com/bangueco/event-registration-system"
-  },
-  {
-    url: "/images/projects/weather-app.png",
-    title: "Weather App",
-    description: "A simple personal project that fetches the current weather of a city using some third party weather api. This project is also my first project using webpack, and also helps me understand how async javascript works under the hood.",
-    technologies: [{tech: "HTML"}, {tech: "CSS"}, {tech: "Javascript"}, {tech: "Webpack"}, {tech: "npm"}],
-    repo_url: "https://www.github.com/bangueco/weather-app",
-    live_preview: "https://bangueco.github.io/weather-app"
   }
 ]
