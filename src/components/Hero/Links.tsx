a9196bfb4c09f4fe8ef1766ead9204c1368fc477
bangueco <121280104+bangueco@users.noreply.github.com>
2024-12-22 19:20:01 +0800

    feat: add functionality to download resume

--- src/components/Hero/Links.tsx
+++ src/components/Hero/Links.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import {motion} from 'framer-motion'
+import Link from 'next/link';
 import { FaFacebookF, FaLinkedinIn, FaGithub } from "react-icons/fa6";
 
 export default function Links() {
@@ -11,33 +12,36 @@ export default function Links() {
       animate={{y: 0, opacity: 1, filter: 'blur(0px)'}}
       transition={{delay: 1, duration: 0.4}}
     >
-      <a
+      <Link
         href="https://www.facebook.com/xxxjustentacion"
         target='_blank'
         className="border-2 p-2 rounded-lg border-light-primary dark:border-dark-primary text-sm lg:text-md hover:bg-light-primary dark:hover:bg-dark-primary hover:text-white dark:hover:text-black"
       >
         <FaFacebookF size={15}/>
-      </a>
-      <a
+      </Link>
+      <Link
         href="https://www.linkedin.com/in/justine-ivan-gueco-4a8a3a312/"
         target='_blank'
         className="border-2 p-2 rounded-lg border-light-primary dark:border-dark-primary text-sm lg:text-md hover:bg-light-primary dark:hover:bg-dark-primary hover:text-white dark:hover:text-black"
       >
         <FaLinkedinIn size={15}/>
-      </a>
-      <a
+      </Link>
+      <Link
         href="https://www.github.com/bangueco"
         target='_blank'
         className="border-2 p-2 rounded-lg border-light-primary dark:border-dark-primary text-sm lg:text-md hover:bg-light-primary dark:hover:bg-dark-primary hover:text-white dark:hover:text-black"
       >
         <FaGithub size={15}/>
-      </a>
-      <a
-        href="https://www.facebook.com"
+      </Link>
+      <Link
+        href="/Resume.pdf"
+        rel="noopener noreferrer"
+        download
+        target="_blank"
         className="border-2 p-2 rounded-lg border-light-primary dark:border-dark-primary text-sm lg:text-md hover:bg-light-primary dark:hover:bg-dark-primary hover:text-white dark:hover:text-black"
       >
         Download CV
-      </a>
+      </Link>
     </motion.div>
   )
 }
