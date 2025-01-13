995b036c3bef417fe7df3beadc1af7aa27ca7ff7
bangueco <121280104+bangueco@users.noreply.github.com>
2025-01-13 17:08:58 +0800

    feat: remove resume for privacy reasons

--- src/components/Projects/ListOfProjects.tsx
+++ src/components/Projects/ListOfProjects.tsx
@@ -7,10 +7,17 @@ export default function ListOfProjects() {
   return (
     <>
       <h1 className="text-light-primary dark:text-dark-primary text-3xl md:text-4xl lg:text-5xl font-bold">My projects</h1>
-      <p className="text-center">
-        A list of my personal and school projects, more info can be found at my <a className="text-light-primary font-bold underline dark:text-dark-primary" href="https://www.github.com/bangueco">Github</a>
+      <p className="text-justify text-sm md:text-md p-3 max-sm:w-full sm:w-3/4 md:w-2/3">
+        A list of my personal, school and learning projects. This projects are part of my journey as a self taught college student aspiring to become a developer. These projects are hosted on my&nbsp;
+        <a
+          className="text-light-primary font-bold underline dark:text-dark-primary"
+          href="https://www.github.com/bangueco"
+        >
+          Github
+        </a>
+        .&nbsp;More information about the projects I have created, you can check my github! &gt;_&lt;
       </p>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-auto"
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-auto pt-10"
       >
         {
           projects.map((project, index) => (
