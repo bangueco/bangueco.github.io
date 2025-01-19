6181b37238842ec600ebe87f2a3a344ae2cebba7
bangueco <121280104+bangueco@users.noreply.github.com>
2025-01-19 10:44:29 +0800

    feat: remove education and add contact and fix some fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     <div className="px-8 py-8 lg:px-16 lg:py-16 lg:flex lg:justify-between lg:h-full lg:relative overflow-auto">
       <header className="flex flex-col gap-3 lg:h-full lg:sticky lg:top-0 lg:w-[50%]">
         <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-slate-200">Justine Ivan Gueco</h1>
-        <h2 className="text-sm sm:text-md md:text-lg font-medium text-slate-200">Full Stack Developer | Mobile Developer</h2>
-        <p className="max-w-xs text-xs sm:text-sm md:text-base">I specialize in building mobile and web applications from scratch</p>
+        <h2 className="text-sm sm:text-lg md:text-xl font-medium text-slate-200">Full Stack Developer | Mobile Developer</h2>
+        <p className="max-w-xs text-sm md:text-base leading-relaxed">I specialize in building mobile and web applications from scratch</p>
         <div className="mt-5 flex gap-3">
           <Link className="hover:text-slate-200" href="https://github.com/bangueco" target="_blank">
             <FaGithub size={24} />
@@ -25,51 +25,50 @@ export default function Home() {
       </header>
       <main className="pt-12 lg:pt-0 lg:w-[50%]">
         <section id="about" className="mb-24">
-          <h1 className="mb-8 font-bold text-slate-200 text-2xl sm:text-3xl md:text-4xl"># ABOUT ME</h1>
+          <h1 className="mb-8 font-bold text-slate-200 text-2xl sm:text-3xl md:text-4xl">👨🏻‍🎓 ABOUT ME</h1>
           <div className="space-y-5">
             <p className="text-justify text-sm md:text-base leading-relaxed">
-              I&apos;m a self-taught college student from <span className="text-slate-200 font-medium">Philippines</span> with
+              I&apos;m a self-taught college student from Philippines with
               a deep passion for technology and continuous learning. I specialize in building innovative applications
-              for <span className="text-slate-200 font-medium">mobile and web platforms</span>, transforming ideas into
+              for mobile and web platforms, transforming ideas into
               functional and user-friendly solutions.
             </p>
             <p className="text-justify text-sm md:text-base leading-relaxed">
-              My journey began in <span className="text-slate-200 font-medium">2022</span> when I started learning web
-              development through online resources. Starting with frontend basics, I progressively expanded into backend
-              development with PHP and Laravel. Eventually I transitioned to <span className="text-slate-200 font-medium">Fullstack Development</span> and JavaScript ecosystem.
+              My journey started in my sophomore year (2022) in college when i finally have my own laptop.
+              I started learning through online resources and tutorials, and eventually started building my own projects.
+              As the years went by, I have gained experience in various technologies and frameworks, and have worked on
+              several projects that have helped me grow as a developer as i am today.
             </p>
             <p className="text-justify text-sm md:text-base leading-relaxed">
-              As the <span className="text-slate-200 font-medium">lead developer</span> for our capstone project (2024),
-              I embraced the challenge of mobile development with React Native and Express.js, showcasing my ability to
-              adapt and learn new technologies quickly.
+              Having a strong foundation in both front-end and back-end development especially in mobile development, I am confident
+              in my ability to build applications from scratch, learn new technologies, adapt to new environments and face some challenges.
             </p>
             <p className="text-justify text-sm md:text-base leading-relaxed">
               When I&apos;m not coding, I enjoy playing video games, watching movies and tv series, listening to music, and lifting some weights at the gym.
             </p>
           </div>
           <div className="mt-12">
-            <h2 className="mb-8 font-bold text-slate-200 text-xl sm:text-2xl md:text-3xl text-center">TECHNICAL SKILLS</h2>
-            <div className="grid grid-cols-4 md:grid-cols-6 lg:grid-cols-9">
-              {
-                techStack.map((tech, index) => (
-                  <div
-                    className='flex flex-col justify-center items-center p-2 gap-3 rounded-lg hover:bg-neutral-800/50 transition-colors cursor-pointer' key={index}
-                  >
-                    <Image
-                      width={0}
-                      height={0}
-                      className='h-10 w-10'
-                      src={tech.src}
-                      alt={tech.alt}
-                    />
-                    <p className='text-xs'>{tech.alt}</p>
-                  </div>
-                ))
-              }
+            <h2 className="mb-8 font-bold text-slate-200 text-xl sm:text-2xl md:text-3xl text-center">🖥️ TECHNICAL SKILLS</h2>
+            <div className="grid grid-cols-4 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-7 gap-2">
+              {techStack.map((tech, index) => (
+                <div
+                  className="flex flex-col justify-center items-center p-3 gap-2 border border-zinc-900 rounded-lg hover:bg-neutral-800/50 transition-colors cursor-pointer"
+                  key={index}
+                >
+                  <Image
+                    width={0}
+                    height={0}
+                    className="w-8 h-8 sm:w-10 sm:h-10"
+                    src={tech.src}
+                    alt={tech.alt}
+                  />
+                  <p className="text-[10px] sm:text-xs md:text-sm text-center">{tech.alt}</p>
+                </div>
+              ))}
             </div>
           </div>
           <div className="mt-12">
-            <h2 className="mb-8 font-bold text-slate-200 text-xl sm:text-2xl md:text-3xl text-center">EXPERIENCE</h2>
+            <h2 className="mb-8 font-bold text-slate-200 text-xl sm:text-2xl md:text-3xl text-center">💼 EXPERIENCE</h2>
             <div>
               <div className="mb-4 hover:bg-neutral-800/50 border border-zinc-900 transition-colors p-3 rounded-lg cursor-pointer">
                 <h3 className="text-slate-200 font-bold text-sm sm:text-md md:text-lg">Hire Me So I Could Get Experience</h3>
@@ -77,22 +76,9 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <div className="mt-12">
-            <h2 className="mb-8 font-bold text-slate-200 text-xl sm:text-2xl md:text-3xl text-center">EDUCATION</h2>
-            <div>
-              <div className="mb-4 hover:bg-neutral-800/50 border border-zinc-900 transition-colors p-3 rounded-lg cursor-pointer">
-                <h3 className="text-slate-200 font-bold text-sm sm:text-md md:text-lg">Eastwoods Professional College of Science and Technology</h3>
-                <p className="text-xs sm:text-sm md:text-base">BS in Information Technology (2021 - 2025)</p>
-              </div>
-              <div className="hover:bg-neutral-800/50 border border-zinc-900 transition-colors p-3 rounded-lg cursor-pointer">
-                <h3 className="text-slate-200 font-bold text-sm sm:text-md md:text-lg">ACLC College of Balanga</h3>
-                <p className="text-xs sm:text-sm md:text-base">TVL-ICT (2019 - 2021)</p>
-              </div>
-            </div>
-          </div>
         </section>
         <section id="projects" className="mb-24">
-          <h1 className="mb-8 font-bold text-slate-200 text-2xl sm:text-3xl md:text-4xl"># FEATURED PROJECTS</h1>
+          <h1 className="mb-8 font-bold text-slate-200 text-2xl sm:text-3xl md:text-4xl">📌 FEATURED PROJECTS</h1>
           <div className="grid grid-cols-1 gap-5">
             {
               projects.map((project, index) => (
@@ -115,7 +101,25 @@ export default function Home() {
               ))
             }
           </div>
-          <p className="text-sm sm:text-base text-center sm:text-left mt-10">More of my projects can be found at my <Link className="hover:text-slate-200 font-medium underline" href="https://github.com/bangueco">Github</Link>.</p>
+          <p className="text-sm sm:text-base text-center sm:text-left mt-5">More of my projects can be found at my <Link className="hover:text-slate-200 font-medium underline" href="https://github.com/bangueco">Github</Link>.</p>
+        </section>
+        <section id="contact">
+          <h1 className="mb-8 font-bold text-slate-200 text-2xl sm:text-3xl md:text-4xl">📲 CONNECT WITH ME</h1>
+          <p className="text-justify text-sm md:text-base leading-relaxed">
+            If you have any questions, inquiries, or just want to say hello, feel free to reach out to me through the following platforms provided <span className="lg:hidden">below</span><span className="hidden lg:inline-block">on the side</span>. I&apos;ll try to get back to you as soon as possible.
+            I don&apos;t provide my email address here to avoid potential spam. Thank you for understanding.
+          </p>
+          <div className="mt-5 flex flex-col gap-3 lg:hidden">
+            <Link className="hover:text-slate-200 flex items-center gap-2 text-sm md:text-base" href="https://github.com/bangueco" target="_blank">
+              <FaGithub size={24} /> Visit my Github
+            </Link>
+            <Link className="hover:text-slate-200 flex items-center gap-2 text-sm md:text-base" href="https://www.linkedin.com/in/justine-ivan-gueco-4a8a3a312/" target="_blank">
+              <FaLinkedin size={24} />Connect with me on LinkedIn
+            </Link>
+            <Link className="hover:text-slate-200 flex items-center gap-2 text-sm md:text-base" href="https://www.instagram.com/aybancaughtin4k/" target="_blank">
+              <FaInstagram size={24} />Become mutuals in Instagram
+            </Link>
+          </div>
         </section>
         <section className="mt-24 py-8">
           <div className="max-w-6xl mx-auto px-8 text-center text-slate-300 text-sm">
